4e60c7a9c46bf1f89dc44d1126c6cb72f51365a0
regilkent123 <regilsembrino@gmail.com>
2018-02-23 17:29:30 +0800

    hehe

--- app/src/main/java/com/ruzieljonm/fitbud/SearchActivity.java
+++ app/src/main/java/com/ruzieljonm/fitbud/SearchActivity.java
@@ -10,8 +10,5 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        //REGILZ
-
-        //oh god
     }
 }
